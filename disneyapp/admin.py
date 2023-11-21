c286f452700d18bc4f79612aabe034957062e795
Isariyafxk <100127365+Isariyafxk@users.noreply.github.com>
2023-11-21 15:49:47 +0000

    finish database and try to use data

--- disneyapp/admin.py
+++ disneyapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from disneyapp.models import *
 # Register your models here.
+
+@admin.register(DisneylandReview)
+class Disneyadmin(admin.ModelAdmin):
+    pass
--- disneyapp/views.py
+++ disneyapp/views.py
@@ -1,4 +1,34 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import pandas as pd
+from .models import *
 
-# Create fuctions your views here.
+def import_data_csv(request):
+    csv_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS4EPpyFHzOACG9rfzqk3bJ0PbCawzX24IgYrYptGgi0cvDo4IH7frGmYuNBdLBzYuW0MHxv8EYoBDA/pub?output=csv'
+    df = pd.read_csv(csv_url)
+    data_sets = df[["Review_ID","Rating","Review_Text","Branch"]]
+    sucesss = []
+    errors = []
+    for index, row in data_sets.iterrows():
+        instance = DisneylandReview(
+            review_id = int(row['Review_ID']),
+            rating = int(row['Rating']),
+            text = row['Review_Text'],
+            branch = row['Branch'],
+        )
+        try:
+            instance.save()
+            sucesss.append(index)
+        except:
+            errors.append(index)
+    return JsonResponse({"success_indexes":sucesss,"error_index":errors})
 
+def get_disney_data(request):
+    comments =  DisneylandReview.objects.all().values()
+    df = pd.DataFrame(comments)
+    branch = df['branch']
+    hongkong_data = df[df['branch'] == 'Disneyland_HongKong']
+    hongkong_text = hongkong_data[['review_id','text']]
+    print(hongkong_text)
+
+   
