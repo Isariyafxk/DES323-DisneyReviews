1c7a5430def9b6de8248b789126749b562b0137c
Isariyafxk <100127365+Isariyafxk@users.noreply.github.com>
2023-11-21 12:56:01 +0000

    geberate disneyapp

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -18,12 +18,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from disneyapp import views as disneyapp_views
+
 from hello_world.core import views as core_views
 
 urlpatterns = [
     path("", core_views.index),
     path("admin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
+    path("disneyapp/", disneyapp_views.data_index_view)
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
