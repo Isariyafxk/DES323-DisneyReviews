22421801f1015278a46da0cf164202e2e483e5f3
Isariyafxk <100127365+Isariyafxk@users.noreply.github.com>
2023-11-21 17:31:51 +0000

    sentiment text#1

--- disneyapp/views.py
+++ disneyapp/views.py
@@ -3,6 +3,10 @@ from django.http import JsonResponse
 import pandas as pd
 from .models import *
 
+#  nltk - text sentiment analysis
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+
 def import_data_csv(request):
     csv_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS4EPpyFHzOACG9rfzqk3bJ0PbCawzX24IgYrYptGgi0cvDo4IH7frGmYuNBdLBzYuW0MHxv8EYoBDA/pub?output=csv'
     df = pd.read_csv(csv_url)
@@ -23,12 +27,21 @@ def import_data_csv(request):
             errors.append(index)
     return JsonResponse({"success_indexes":sucesss,"error_index":errors})
 
+#text analysis function-> Negative(-1 - -0.25) Neutral(-0.25 - 0) Positive(+1)
+nltk.download('vader_lexicon')
+
+def sentiment(text):
+    text_score = SentimentIntensityAnalyzer().polarity_scores(text)
+    return print("score = ",text_score['compound'])
+
 def get_disney_data(request):
-    comments =  DisneylandReview.objects.all().values()
-    df = pd.DataFrame(comments)
-    branch = df['branch']
-    hongkong_data = df[df['branch'] == 'Disneyland_HongKong']
-    hongkong_text = hongkong_data[['review_id','text']]
-    print(hongkong_text)
+    review_texts =  DisneylandReview.objects.all().values()
+    df = pd.DataFrame(review_texts)
+    df['sentiment'] = df['text'].apply(sentiment)
+    print(df[['id', 'branch', 'text', 'sentiment']])
+    # branch = df['branch']
+    # hongkong_data = df[df['branch'] == 'Disneyland_Paris']
+    # hongkong_text = hongkong_data[['review_id','text']]
+    # print(hongkong_text)
 
    
