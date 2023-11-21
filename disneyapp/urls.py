311c5e070e53aa2910eb33253d65f95a31228baf
Isariyafxk <100127365+Isariyafxk@users.noreply.github.com>
2023-11-21 22:14:29 +0000

    finish sentiment

--- disneyapp/urls.py
+++ disneyapp/urls.py
@@ -3,5 +3,5 @@ from disneyapp import views
 
 urlpatterns = [
     path("importdata",views.import_data_csv),
-    path("alldata",views.generate_catagories),
+    path("alldata",views.sentiment_HongKong)
 ]
